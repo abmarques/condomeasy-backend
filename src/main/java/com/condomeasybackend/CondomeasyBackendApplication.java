e3a0da0a71b0bd43c531a68ae922f989c39070c9
unknown <abmarques.si@gmail.com>
2021-05-17 20:25:35 -0300

    Backend project creation

---
+++ src/main/java/com/condomeasybackend/CondomeasyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.condomeasybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondomeasyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondomeasyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/condomeasybackend/CondomeasyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condomeasybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondomeasyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
