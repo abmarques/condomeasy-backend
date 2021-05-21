eac41113b40f82663bb3595b99e84083eb37e5aa
unknown <abmarques.si@gmail.com>
2021-05-21 00:11:08 -0300

    Condominium endpoint implementation

--- src/main/java/com/condomeasy/backend/model/Condominium.java
+++ src/main/java/com/condomeasy/backend/model/Condominium.java
@@ -1,9 +1,7 @@
 package com.condomeasy.backend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -17,37 +15,48 @@ public class Condominium {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
     private Integer id;
 
     @Column(name = "nome")
+    @JsonProperty("name")
     private String name;
 
     @Column(name = "cnpj")
+    @JsonProperty("cnpj")
     private String cnpj;
 
     @Column(name = "logradouro")
+    @JsonProperty("adress")
     private String adress;
 
     @Column(name = "complemento")
+    @JsonProperty("complement")
     private String complement;
 
     @Column(name = "numero")
+    @JsonProperty("number")
     private Integer number;
 
     @Column(name = "bairro")
+    @JsonProperty("neighborhood")
     private String neighborhood;
 
     @Column(name = "cidade")
+    @JsonProperty("city")
     private String city;
 
     @Column(name = "uf")
+    @JsonProperty("uf")
     private String uf;
 
     @Column(name = "localizacao_x")
+    @JsonProperty("localizationX")
     private String localizationX;
 
     @Column(name = "localizacao_y")
+    @JsonProperty("localizationY")
     private String localizationY;
 
 }
