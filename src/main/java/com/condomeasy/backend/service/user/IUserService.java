d05ddbc1af9c70e56d6b2e42730d2e3ff93f4495
mathews <mathews.alves.2019@outlook.com>
2021-05-23 17:29:42 -0300

    resolved comments in pr

--- src/main/java/com/condomeasy/backend/service/user/IUserService.java
+++ src/main/java/com/condomeasy/backend/service/user/IUserService.java
@@ -1,15 +1,10 @@
 package com.condomeasy.backend.service.user;
 
-import javax.mail.internet.AddressException;
-
-import com.condomeasy.backend.exception.EmailAlreadyExistsException;
-import com.condomeasy.backend.exception.InvalidCPFException;
-import com.condomeasy.backend.exception.UsernameAlreadyExistsException;
 import com.condomeasy.backend.model.User;
 
 public interface IUserService {
 
-	User save(User u) throws AddressException, EmailAlreadyExistsException, UsernameAlreadyExistsException, InvalidCPFException;
+	User save(User u);
 	User findById(Integer id);
 	User findByUsername(String username);
 	
