ba2b5d8b29e51103f8df8659f03d9e96837ee8d8
mathews <mathews.alves.2019@outlook.com>
2021-05-27 20:44:54 -0300

    correcting message

--- src/main/java/com/condomeasy/backend/dto/ProfileDTO.java
+++ src/main/java/com/condomeasy/backend/dto/ProfileDTO.java
@@ -14,6 +14,6 @@ import lombok.NoArgsConstructor;
 public class ProfileDTO {
 
 	private Integer id;
-	@NotEmpty(message = "Informe o email do perfil.")
+	@NotEmpty(message = "Informe o nome do perfil.")
 	private String name;
 }
