69dc9dd6a8e5459e4a53ec2e9495738fa91c90b5
abmarques <abmarques.si@gmail.com>
2021-11-04 14:43:22 -0300

    Removed unused lines

--- src/main/java/com/condomeasy/backend/controller/AuthenticationController.java
+++ src/main/java/com/condomeasy/backend/controller/AuthenticationController.java
@@ -1,20 +1,14 @@
 package com.condomeasy.backend.controller;
 
-import com.condomeasy.backend.exception.BusinessException;
-import com.condomeasy.backend.service.impl.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.condomeasy.backend.dto.AuthenticationRequestDTO;
 import com.condomeasy.backend.dto.AuthenticationResponseDTO;
 import com.condomeasy.backend.service.impl.DefaultUserDetailsService;
 import com.condomeasy.backend.util.JwtUtil;
-
-import static com.condomeasy.backend.constant.MessageBundle.INVALID_CREDENTIALS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
@@ -24,9 +18,6 @@ public class AuthenticationController {
 
     @Autowired
     private DefaultUserDetailsService defaultUserDetailsService;
-
-    @Autowired
-    private UserService userService;
 
     @PostMapping("/authenticate")
     public AuthenticationResponseDTO authenticate(@RequestBody AuthenticationRequestDTO authenticationRequestDto) throws Exception {
